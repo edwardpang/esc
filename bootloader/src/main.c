b407331ad2780eadd03e4797050fcd729ae941cf
Edward Pang <edwardpang@edwardpang.com>
2015-03-09 00:05:07 +0800

    Add basic init function into bootloader

--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -1,5 +1,21 @@
+#pragma sfr
+#pragma DI
+#pragma EI
+#pragma NOP
+#pragma HALT
+#pragma STOP
+
 #include "fsl.h"
 #include "fsl_types.h"
+
+void init (void) {
+	PIOR0 = 0x00U;
+	PIOR1 = 0x00U;
+	
+	CRC0CTL = 0x00U;
+	IAWCTL = 0x00U;
+	PMS = 0x00U;
+}
 
 void main (void) {
 }
